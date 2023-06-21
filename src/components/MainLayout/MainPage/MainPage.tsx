e7a0d12ed2e4898e0b859687929fddfe9524a788
Denis Podsobilov <dpodsobilov29@gmail.com>
2023-06-21 14:04:10 +0400

    fixing header section

--- src/components/MainLayout/MainPage/MainPage.tsx
+++ src/components/MainLayout/MainPage/MainPage.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { MainPageStyled } from './MainPageStyled';
 import TextWrapper from "./TextWrapper/TextWrapper";
-import {PiperNetPic} from "./PiperNetPic";
+import PiperNetPic from "./PiperNetPic/PiperNetPic";
+import styled from "styled-components";
 
-
+export const H1 = styled.h1<{ primary: boolean }>`
+  color: ${(props) => props.primary ? 'white' : '#007765'};
+  font-family: Roboto,sans-serif;
+  font-size: 3vw;
+`;
 export default function MainPage() {
     return (
       <MainPageStyled>
-          <PiperNetPic />
+          <PiperNetPic>
+              <H1 primary={true}>PiperNet Is Here</H1>
+          </PiperNetPic>
           <TextWrapper />
       </MainPageStyled>
     );
---
+++ src/components/MainLayout/MainPage/PiperNetPic/PiperNetPicStyled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import image from '../../../../asserts/header/header-image.png';
+
+export const PiperNetPicStyled = styled.div`
+  background-image: url(${image});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 25vh;
+  //position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
