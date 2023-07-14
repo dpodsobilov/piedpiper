b71c3a853104679bc260ccaf232f6dda868c1975
Денис Подсобилов <dpodsobilov29@gmail.com>
2023-07-14 13:22:19 +0400

    creating footer

---
+++ src/components/MainLayout/MainPage/Footer/FooterStyled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FooterStyled = styled.div`
+  background-color: #333333;
+  margin: auto;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  color: white;
+  justify-content: center;
+  align-items: center;
+`;
--- src/components/styles/P.tsx
+++ src/components/styles/P.tsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
-export const P = styled.p<{ bold?: boolean, italic?: boolean, color?: string, paddingBottom?: string }>`
+export const P = styled.p<{ 
+  bold?: boolean,
+  italic?: boolean, 
+  color?: string, 
+  paddingBottom?: string,
+  fontSize?: string
+  }>`
   color: ${(props) => props.color ? props.color.toString() : 'black'};
   font-family: Roboto,sans-serif;
-  font-size: 1.2vw;
+  font-size: ${(props) => props.fontSize ? props.fontSize : '1.2vw'};
   margin-bottom: ${(props) => props.paddingBottom ? props.paddingBottom : '1em'};
   margin-top: 1vh;
   font-weight: ${(props) => props.bold ? 'bold' : 'normal'};
